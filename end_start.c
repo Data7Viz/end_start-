9543fcfd7fe127b3a56c337c92e6df3b940817b2
Data7Viz <data7vizz@gmail.com>
2026-04-19 20:47:26 +0800

    черновик

---
+++ end_start.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#define HZ 1`000`000
+int main ()
+{
+	double start = clock();
+		// 
+		//подопытный
+		//
+	double end = clock();
+	double tic = (double)(end - stert) \ HZ;
+}
+
+
